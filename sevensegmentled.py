d53739786870a22eb250a3259566c05dc310e180
codepoet80 <jon.wise@jonandnic.com>
2020-02-06 15:44:08 +0000

    bug fix seven seg led

--- sevensegmentled.py
+++ sevensegmentled.py
@@ -5,22 +5,23 @@ GPIO.setwarnings(False)
 
 # Define LED output segments
 #	TODO: The pins should be passed in from the main program as a constructor argument or property
-p1NumSegPins = [17,5,19,27,22,13,6]    #aka red team
+p1NumSegPins = [17,5,19,20,22,13,6]    #aka red team
 p2NumSegPins = [18,25,26,24,23,16,12]   #aka blue team
 # Number shape arrays (each led number is an array within the number shape array
 # the led number array (the inner array) will align with the NumSegPin array positions
 # so we know which pin to set to the corresponding value in the inner array.
 numShapeValues = []
+
 numShapeValues.append([0,1,0,0,0,0,0]) #0
-numShapeValues.append([1,1,1,0,1,0,1]) #1
-numShapeValues.append([0,0,0,0,1,1,0]) #2
-numShapeValues.append([0,0,0,0,1,0,1]) #3
-numShapeValues.append([1,0,1,0,0,0,1]) #4
-numShapeValues.append([0,0,0,1,0,0,1]) #5
-numShapeValues.append([0,0,0,1,0,0,0]) #6
-numShapeValues.append([0,1,1,0,1,0,1]) #7
+numShapeValues.append([1,1,1,1,0,1,0]) #1
+numShapeValues.append([0,0,0,1,0,0,1]) #2
+numShapeValues.append([0,0,0,1,0,1,0]) #3
+numShapeValues.append([1,0,1,0,0,1,0]) #4
+numShapeValues.append([0,0,0,0,1,1,0]) #5
+numShapeValues.append([0,0,0,0,1,0,0]) #6
+numShapeValues.append([0,1,1,1,0,1,0]) #7
 numShapeValues.append([0,0,0,0,0,0,0]) #8
-numShapeValues.append([0,1,0,0,0,0,0]) #9
+numShapeValues.append([0,0,0,0,0,1,0]) #9
 
 #init our pins based on variables and arrays above
 for numSegPin in p1NumSegPins:
