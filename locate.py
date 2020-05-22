a3a04ff2ff1c677a021b28ddd08b9760adeefbf4
codepoet80 <jonathan.m.wise@gmail.com>
2020-05-22 18:58:54 -0400

    add 11

--- locate.py
+++ locate.py
@@ -9,7 +9,8 @@ x=20
 y=19
 
 # A Simple clear screen command for this DEMO...
-for n in range(0, 64, 1): print("\r\n")
+def clear():
+    for n in range(0, 64, 1): print("\r\n")
 
 # This function is just basic for this DEMO but shows the power of the ANSI _Esc_ codes...
 def locate(user_string="$VER: Locate_Demo.py_Version_0.00.10_(C)2007-2012_B.Walker_G0LCU.", x=0, y=0):
@@ -26,10 +27,19 @@ def locate(user_string="$VER: Locate_Demo.py_Version_0.00.10_(C)2007-2012_B.Walk
 	# Plot the user_string at the starting at position HORIZ, VERT...
 	print("\033["+VERT+";"+HORIZ+"f"+user_string)
 
-locate("███", 5, 2)
-locate("███", 5, 3)
-locate("███", 5, 4)
-locate("███", 5, 5)
-locate("███", 5, 6)
-locate("███", 5, 7)
-locate("███", 5, 8)
+def score_one(player):
+    if player == 1:
+        left = 18
+    else:
+        left = 48
+    locate("███", left, 2)
+    locate("███", left, 3)
+    locate("███", left, 4)
+    locate("███", left, 5)
+    locate("███", left, 6)
+    locate("███", left, 7)
+    locate("███", left, 8)
+
+clear()
+score_one(1)
+score_one(2)
